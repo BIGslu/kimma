01d50f8ee0ddc4920663f19d3ac8578915ff6f7e
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2021-07-22 09:41:42 -0700

    feat: separate contrasts in kmFit summary

--- R/summarise_kmFit.R
+++ R/summarise_kmFit.R
@@ -4,10 +4,11 @@
 #'
 #' @param fdr data.frame output by kimma::extract_lmFit( )
 #' @param fdr.cutoff numeric vector of FDR cutoffs to summarise at
+#' @param contrast logical if should separate summary by pairwise contrasts within variables
 #' @param FCgroup logical if should separate summary by up/down fold change groups
 #' @param intercept logical if should include intercept variable in summary
 #'
-#' @return Data frame with total significant genes for each variable are various FDR cutoffs
+#' @return Data frame with total significant genes for each variable at various FDR cutoffs
 #' @export
 #'
 #' @examples
@@ -24,7 +25,7 @@
 #'
 
 summarise_kmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
-                            FCgroup = FALSE, intercept = FALSE){
+                            contrast = FALSE, FCgroup = FALSE, intercept = FALSE){
   estimate <- variable <- FDR <- gene <- group <- n <- model <- NULL
   if(intercept){
     fdr.filter <- fdr %>%
@@ -35,7 +36,31 @@ summarise_kmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
       dplyr::mutate(FCgroup = ifelse(estimate<0,"down","up"))
   }
 
-  if(FCgroup){
+  if(FCgroup & contrast){
+    #Blank df for results
+    result <- data.frame()
+
+    for(FDR.i in fdr.cutoff){
+      name.fdr <- paste("fdr",FDR.i, sep="_")
+      #Calculate total, nonredundant signif genes at different levels
+      total.temp <- fdr.filter %>%
+        dplyr::filter(FDR <= FDR.i) %>%
+        dplyr::distinct(model, gene, contrast, FCgroup) %>%
+        dplyr::count(model, FCgroup, .drop = FALSE) %>%
+        dplyr::mutate(variable = "total (nonredundant)")
+
+      #Summarize signif genes per variable at various levels
+      group.temp <- fdr.filter %>%
+        dplyr::filter(FDR <= FDR.i) %>%
+        dplyr::count(model, variable, contrast, FCgroup, .drop = FALSE)
+
+      result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
+        dplyr::mutate(group = name.fdr)
+
+      result <- dplyr::bind_rows(result, result.temp)
+    }
+  }
+  else if(FCgroup){
     #Blank df for results
     result <- data.frame()
 
@@ -58,7 +83,30 @@ summarise_kmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
 
       result <- dplyr::bind_rows(result, result.temp)
     }
-  } else {
+  } else if(contrast){
+    #Blank df for results
+    result <- data.frame()
+
+    for(FDR.i in fdr.cutoff){
+      name.fdr <- paste("fdr",FDR.i, sep="_")
+      #Calculate total, nonredundant signif genes at different levels
+      total.temp <- fdr.filter %>%
+        dplyr::filter(FDR <= FDR.i) %>%
+        dplyr::distinct(model, gene) %>%
+        dplyr::mutate(variable = "total (nonredundant)") %>%
+        dplyr::count(model, variable, .drop = FALSE)
+
+      #Summarize signif genes per variable at various levels
+      group.temp <- fdr.filter %>%
+        dplyr::filter(FDR <= FDR.i) %>%
+        dplyr::count(model, variable, contrast, .drop = FALSE)
+
+      result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
+        dplyr::mutate(group = name.fdr)
+
+      result <- dplyr::bind_rows(result, result.temp)
+    }
+    } else {
     #Blank df for results
     result <- data.frame()
 
