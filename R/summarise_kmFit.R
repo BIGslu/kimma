d26b71c7553c7a4a6e831d6301edd4b409c3da90
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2022-03-30 16:06:19 -0700

    Add explicit contrasts to summarise_ fxns

--- R/summarise_kmFit.R
+++ R/summarise_kmFit.R
@@ -2,10 +2,9 @@
 #'
 #' Summarise number of significant genes at various FDR cutoffs. Can split by up/down fold change as well.
 #'
-#' @param fdr data.frame output by kimma::kmFit( )
+#' @param fdr data.frame output by kimma::kmFit( ). Main model or contrasts accepted
 #' @param fdr.cutoff numeric vector of FDR cutoffs to summarise at
-#' @param p.cutoff numeric vector of P-value cutodds to summarise at. No FDR summary given if p.cutoff is provided
-#' @param contrast logical if should separate summary by pairwise contrasts within variables
+#' @param p.cutoff numeric vector of P-value cutoffs to summarise at. No FDR summary given if p.cutoff is provided
 #' @param FCgroup logical if should separate summary by up/down fold change groups
 #' @param intercept logical if should include intercept variable in summary
 #'
@@ -16,34 +15,28 @@
 #' # Run kimma model
 #' model_results <- kmFit(dat = example.voom,
 #'       kin = example.kin,
-#'       run.lme = TRUE, run.lmerel=TRUE,
+#'       run.lme = TRUE, run.lmerel=TRUE, run.contrast=TRUE,
 #'       subset.genes = c("ENSG00000250479","ENSG00000250510","ENSG00000255823"),
 #'       model = "~ virus + (1|ptID)")
 #'
 #' # Summarise results
 #' summarise_kmFit(fdr = model_results$lmerel, fdr.cutoff = c(0.01, 0.5),
 #'                 FCgroup = TRUE)
-#' summarise_kmFit(fdr = model_results$lmerel, fdr.cutoff = c(0.001))
+#' summarise_kmFit(fdr = model_results$lme.contrast, fdr.cutoff = c(0.01, 0.5),
+#'                 FCgroup = FALSE)
+#'
+#' #No significant genes. No run
+#' ## summarise_kmFit(fdr = model_results$lmerel, fdr.cutoff = c(0.001))
 #'
 
 summarise_kmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
-                            p.cutoff = NULL,
-                            contrast = FALSE, FCgroup = FALSE, intercept = FALSE){
-  estimate <- variable <- FDR <- gene <- group <- n <- model <- fdr.var <- NULL
-
-  if(intercept){
-    fdr.filter <- fdr %>%
-      dplyr::mutate(variable=factor(variable)) %>%
-      dplyr::mutate(FCgroup = ifelse(estimate<0,"down","up"),
-                    FCgroup = factor(FCgroup),
-                    model = factor(model))
-
+                            p.cutoff = NULL, FCgroup = FALSE, intercept = FALSE){
+  group <- n <- variable <- fdr.var <- estimate <- gene <- contrast_ref <- contrast_lvl <- NULL
+  if(intercept | "contrast_ref" %in% colnames(fdr)){
+    fdr.filter <- fdr
   } else{
     fdr.filter <- dplyr::filter(fdr, variable != '(Intercept)') %>%
-      dplyr::mutate(variable=factor(variable)) %>%
-      dplyr::mutate(FCgroup = ifelse(estimate<0,"down","up"),
-                    FCgroup = factor(FCgroup),
-                    model = factor(model))
+      droplevels()
   }
 
   # Use p-values if specified
@@ -54,122 +47,92 @@ summarise_kmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
     fdr.var <- "FDR"
   }
 
-  if(FCgroup & contrast){
+  if(FCgroup){
+    fdr.filter.FC <- fdr.filter %>%
+      dplyr::mutate(FCgroup = ifelse(estimate < 0, "down", "up"))
     #Blank df for results
     result <- data.frame()
 
     for(FDR.i in fdr.cutoff){
       name.fdr <- paste("fdr",FDR.i, sep="_")
       #Calculate total, nonredundant signif genes at different levels
-      total.temp <- fdr.filter %>%
+      total.temp <- fdr.filter.FC %>%
         dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::distinct(model, gene, contrast, FCgroup) %>%
-        dplyr::count(model, FCgroup, .drop = FALSE) %>%
+        dplyr::distinct(gene, FCgroup) %>%
+        dplyr::count(FCgroup, .drop = FALSE) %>%
         dplyr::mutate(variable = "total (nonredundant)")
 
       #Summarize signif genes per variable at various levels
-      group.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::count(model, variable, contrast, FCgroup, .drop = FALSE)
+      if("contrast_ref" %in% colnames(fdr.filter.FC)){
+        group.temp <- fdr.filter.FC %>%
+          dplyr::filter(get(fdr.var) <= FDR.i) %>%
+          dplyr::count(variable, contrast_ref, contrast_lvl, FCgroup, .drop = FALSE)
 
-      result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
-        dplyr::mutate(group = name.fdr,
-                      contrast = gsub("contrast","",contrast))
+        result.temp <- total.temp %>%
+          dplyr::bind_rows(group.temp) %>%
+          dplyr::mutate(group = name.fdr)
 
-      result <- dplyr::bind_rows(result, result.temp)
-    }
-  } else if(FCgroup){
-    #Blank df for results
-    result <- data.frame()
+        result <- dplyr::bind_rows(result, result.temp)
+      } else{
+        group.temp <- fdr.filter.FC %>%
+          dplyr::filter(get(fdr.var) <= FDR.i) %>%
+          dplyr::count(variable, FCgroup, .drop = FALSE)
 
-    for(FDR.i in fdr.cutoff){
-      name.fdr <- paste("fdr",FDR.i, sep="_")
-      #Calculate total, nonredundant signif genes at different levels
-      total.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::distinct(model, gene, FCgroup) %>%
-        dplyr::count(model, FCgroup, .drop = FALSE) %>%
-        dplyr::mutate(variable = "total (nonredundant)")
-
-      #Summarize signif genes per variable at various levels
-      group.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::count(model, variable, FCgroup, .drop = FALSE)
-
-      result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
-        dplyr::mutate(group = name.fdr)
-
-      result <- dplyr::bind_rows(result, result.temp)
-    }
-  } else if(contrast){
-    #Blank df for results
-    result <- data.frame()
-
-    for(FDR.i in fdr.cutoff){
-      name.fdr <- paste("fdr",FDR.i, sep="_")
-      #Calculate total, nonredundant signif genes at different levels
-      total.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::distinct(model, gene) %>%
-        dplyr::mutate(variable = "total (nonredundant)") %>%
-        dplyr::count(model, variable, .drop = FALSE)
-
-
-      #Summarize signif genes per variable at various levels
-      group.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::count(model, variable, contrast, .drop = FALSE)
-
-      result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
-        dplyr::mutate(group = name.fdr,
-                      contrast = gsub("contrast","",contrast))
-
-      result <- dplyr::bind_rows(result, result.temp)
-    }} else {
-    #Blank df for results
-    result <- data.frame()
-
-    for(FDR.i in fdr.cutoff){
-      name.fdr <- paste("fdr",FDR.i, sep="_")
-      #Calculate total, nonredundant signif genes at different levels
-      total.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::distinct(model, gene) %>%
-        dplyr::mutate(variable = "total (nonredundant)") %>%
-        dplyr::count(model, variable, .drop = FALSE)%>%
-        dplyr::mutate(variable = "total (nonredundant)")
-
-      #Summarize signif genes per variable at various levels
-      group.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::count(model, variable, .drop = FALSE)
-
-      #If 0 genes, make zero df
-      if(nrow(total.temp)==0){
-        result.temp <- data.frame(variable=c(unique(fdr.filter$variable),
-                                             "total (nonredundant)"),
-                                  n=0,
-                                  model=unique(fdr.filter$model),
-                                  group=name.fdr)
-      } else {
         result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
           dplyr::mutate(group = name.fdr)
+
+        result <- dplyr::bind_rows(result, result.temp)
       }
-      result <- dplyr::bind_rows(result, result.temp)
     }
+  } else {
+    #Blank df for results
+    result <- data.frame()
+
+    for(FDR.i in fdr.cutoff){
+      name.fdr <- paste("fdr",FDR.i, sep="_")
+      #Calculate total, nonredundant signif genes at different levels
+      total.temp <- fdr.filter %>%
+        dplyr::filter(get(fdr.var) <= FDR.i) %>%
+        dplyr::distinct(gene) %>%
+        dplyr::mutate(variable = "total (nonredundant)") %>%
+        dplyr::count(variable, .drop = FALSE)
+
+      #Summarize signif genes per variable at various levels
+      if("contrast_ref" %in% colnames(fdr.filter)){
+        group.temp <- fdr.filter %>%
+          dplyr::filter(get(fdr.var) <= FDR.i) %>%
+          dplyr::count(variable, contrast_ref, contrast_lvl, .drop = FALSE)
+
+        result.temp <- total.temp %>%
+          dplyr::bind_rows(group.temp) %>%
+          dplyr::mutate(group = name.fdr)
+
+        result <- dplyr::bind_rows(result, result.temp)
+      } else{
+        group.temp <- fdr.filter %>%
+          dplyr::filter(get(fdr.var) <= FDR.i) %>%
+          dplyr::count(variable, .drop = FALSE)
+
+        result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
+          dplyr::mutate(group = name.fdr)
+
+        result <- dplyr::bind_rows(result, result.temp)
+      }
+    }
+  }
+
+  #Stop if not signif
+  if(nrow(result) == 0){
+    stop("No significant genes. Please increase FDR or P-value cutoffs.")
   }
 
   #Format to wide output
-  if("total (nonredundant)" %in% result$variable){
-  result.format <- tidyr::pivot_wider(result, names_from = group, values_from = n) %>%
-    dplyr::mutate(variable = forcats::fct_relevel(variable,
-                                                  "total (nonredundant)",
-                                                  after=Inf)) %>%
-    dplyr::arrange(model, variable)
-  } else{
-    result.format <- tidyr::pivot_wider(result, names_from = group, values_from = n) %>%
-      dplyr::arrange(model, variable)
-  }
+  result.format <- tidyr::pivot_wider(result, names_from = group,
+                                        values_from = n) %>%
+      dplyr::mutate(variable = forcats::fct_relevel(factor(variable),
+                                                    "total (nonredundant)",
+                                                    after=Inf)) %>%
+      dplyr::arrange(variable)
 
   #rename for P-value if specified
   if(!is.null(p.cutoff)) {
--- R/summarise_lmFit.R
+++ R/summarise_lmFit.R
@@ -4,7 +4,7 @@
 #'
 #' @param fdr data.frame output by kimma::extract_lmFit( )
 #' @param fdr.cutoff numeric vector of FDR cutoffs to summarise at
-#' @param p.cutoff numeric vector of P-value cutodds to summarise at. No FDR summary given if p.cutoff is provided
+#' @param p.cutoff numeric vector of P-value cutoffs to summarise at. No FDR summary given if p.cutoff is provided
 #' @param FCgroup logical if should separate summary by up/down fold change groups
 #' @param intercept logical if should include intercept variable in summary
 #'
@@ -21,12 +21,27 @@
 #' # Summarise results
 #' summarise_lmFit(fdr = model_results, fdr.cutoff = c(0.05, 0.5), FCgroup = TRUE)
 #'
+#' # Contrasts model
+#' design <- model.matrix(~ 0 + virus, data = example.voom$targets)
+#' fit <- limma::lmFit(example.voom$E, design)
+#' contrast.mat <- limma::makeContrasts(virusHRV-virusnone, levels = design)
+#' fit <- limma::eBayes(limma::contrasts.fit(fit, contrast.mat))
+#'
+#' ## Get results
+#' model_results <- extract_lmFit(design = design, fit = fit,
+#'                                contrast.mat = contrast.mat)
+#'
+#' # Summarise results
+#' summarise_lmFit(fdr = model_results, fdr.cutoff = c(0.05, 0.5), FCgroup = FALSE)
+#' ## No signif results (not run)
+#' # summarise_lmFit(fdr = model_results, fdr.cutoff = 0.0001, FCgroup = FALSE)
+#'
 
 summarise_lmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
                             p.cutoff = NULL,
                             FCgroup = FALSE, intercept = FALSE){
-  adj.P.Val <- geneName <- group <- n <- variable <- fdr.var <- NULL
-  if(intercept){
+  group <- n <- variable <- fdr.var <- estimate <- gene <- contrast_ref <- contrast_lvl <- NULL
+  if(intercept | "contrast_ref" %in% colnames(fdr)){
     fdr.filter <- fdr
   } else{
     fdr.filter <- dplyr::filter(fdr, variable != '(Intercept)') %>%
@@ -36,12 +51,50 @@ summarise_lmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
   # Use p-values if specified
   if(!is.null(p.cutoff)){
     fdr.cutoff <- p.cutoff
-    fdr.var <- "P.Value"
+    fdr.var <- "pval"
   } else {
-    fdr.var <- "adj.P.Val"
+    fdr.var <- "FDR"
   }
 
-    if(FCgroup){
+  if(FCgroup){
+    fdr.filter.FC <- fdr.filter %>%
+      dplyr::mutate(FCgroup = ifelse(estimate < 0, "down", "up"))
+    #Blank df for results
+    result <- data.frame()
+
+    for(FDR.i in fdr.cutoff){
+      name.fdr <- paste("fdr",FDR.i, sep="_")
+      #Calculate total, nonredundant signif genes at different levels
+      total.temp <- fdr.filter.FC %>%
+        dplyr::filter(get(fdr.var) <= FDR.i) %>%
+        dplyr::distinct(gene, FCgroup) %>%
+        dplyr::count(FCgroup, .drop = FALSE) %>%
+        dplyr::mutate(variable = "total (nonredundant)")
+
+      #Summarize signif genes per variable at various levels
+      if("contrast_ref" %in% colnames(fdr.filter.FC)){
+        group.temp <- fdr.filter.FC %>%
+          dplyr::filter(get(fdr.var) <= FDR.i) %>%
+          dplyr::count(contrast_ref, contrast_lvl, FCgroup, .drop = FALSE)
+
+        result.temp <- total.temp %>%
+          dplyr::rename(contrast_ref=variable) %>%
+          dplyr::bind_rows(group.temp) %>%
+          dplyr::mutate(group = name.fdr)
+
+        result <- dplyr::bind_rows(result, result.temp)
+      } else{
+        group.temp <- fdr.filter.FC %>%
+          dplyr::filter(get(fdr.var) <= FDR.i) %>%
+          dplyr::count(variable, FCgroup, .drop = FALSE)
+
+        result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
+          dplyr::mutate(group = name.fdr)
+
+        result <- dplyr::bind_rows(result, result.temp)
+      }
+    }
+  } else {
     #Blank df for results
     result <- data.frame()
 
@@ -50,59 +103,56 @@ summarise_lmFit <- function(fdr, fdr.cutoff = c(0.05,0.1,0.2,0.3,0.4,0.5),
       #Calculate total, nonredundant signif genes at different levels
       total.temp <- fdr.filter %>%
         dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::distinct(geneName, FCgroup) %>%
-        dplyr::count(FCgroup, .drop = FALSE) %>%
-        dplyr::mutate(variable = "total (nonredundant)")
+        dplyr::distinct(gene) %>%
+        dplyr::mutate(variable = "total (nonredundant)") %>%
+        dplyr::count(variable, .drop = FALSE)
 
       #Summarize signif genes per variable at various levels
-      group.temp <- fdr.filter %>%
-        dplyr::filter(get(fdr.var) <= FDR.i) %>%
-        dplyr::count(variable, FCgroup, .drop = FALSE)
-
-      result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
-        dplyr::mutate(group = name.fdr)
-
-      result <- dplyr::bind_rows(result, result.temp)
-    }
-    } else {
-      #Blank df for results
-      result <- data.frame()
-
-      for(FDR.i in fdr.cutoff){
-        name.fdr <- paste("fdr",FDR.i, sep="_")
-        #Calculate total, nonredundant signif genes at different levels
-        total.temp <- fdr.filter %>%
+      if("contrast_ref" %in% colnames(fdr.filter)){
+        group.temp <- fdr.filter %>%
           dplyr::filter(get(fdr.var) <= FDR.i) %>%
-          dplyr::distinct(geneName) %>%
-          dplyr::mutate(variable = "total (nonredundant)") %>%
-          dplyr::count(variable, .drop = FALSE)
+          dplyr::count(contrast_ref, contrast_lvl, .drop = FALSE)
 
-        #Summarize signif genes per variable at various levels
+        result.temp <- total.temp %>%
+          dplyr::rename(contrast_ref=variable) %>%
+          dplyr::bind_rows(group.temp) %>%
+          dplyr::mutate(group = name.fdr)
+
+        result <- dplyr::bind_rows(result, result.temp)
+      } else{
         group.temp <- fdr.filter %>%
           dplyr::filter(get(fdr.var) <= FDR.i) %>%
           dplyr::count(variable, .drop = FALSE)
 
-        #If 0 genes, make zero df
-        if(nrow(total.temp)==0){
-          result.temp <- data.frame(variable=c(unique(fdr.filter$variable),
-                                               "total (nonredundant)"),
-                                    n=0,
-                                    model=unique(fdr.filter$model),
-                                    group=name.fdr)
-        } else {
-          result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
-            dplyr::mutate(group = name.fdr)
-        }
+        result.temp <- dplyr::bind_rows(total.temp, group.temp) %>%
+          dplyr::mutate(group = name.fdr)
 
         result <- dplyr::bind_rows(result, result.temp)
       }
     }
-  #Format to wide output
-  result.format <- tidyr::pivot_wider(result, names_from = group, values_from = n) %>%
-    dplyr::mutate(variable = forcats::fct_relevel(factor(variable), "total (nonredundant)",
-                                                  after=Inf)) %>%
-    dplyr::arrange(variable)
+  }
 
+  #Stop if not signif
+  if(nrow(result) == 0){
+    stop("No significant genes. Please increase FDR or P-value cutoffs.")
+  }
+
+  #Format to wide output
+  if("contrast_ref" %in% colnames(fdr.filter)){
+    result.format <- tidyr::pivot_wider(result, names_from = group,
+                                        values_from = n) %>%
+      dplyr::mutate(contrast_ref = forcats::fct_relevel(factor(contrast_ref),
+                                                        "total (nonredundant)",
+                                                    after=Inf)) %>%
+      dplyr::arrange(contrast_ref, contrast_lvl)
+  } else{
+    result.format <- tidyr::pivot_wider(result, names_from = group,
+                                        values_from = n) %>%
+      dplyr::mutate(variable = forcats::fct_relevel(factor(variable),
+                                                    "total (nonredundant)",
+                                                    after=Inf)) %>%
+      dplyr::arrange(variable)
+  }
   #rename for P-value if specified
   if(!is.null(p.cutoff)) {
     result.format <- result.format %>%
