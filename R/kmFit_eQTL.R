25570c8b77fc8ce5ee15d597b251d5fe6be8a9a3
Kimberly Dill-McFarland <kdillmcfarland@gmail.com>
2023-05-31 16:13:31 -0700

    kmFit and assoc functions
    Update parameter names from '-' to '_'

--- R/kmFit_eQTL.R
+++ R/kmFit_eQTL.R
@@ -2,10 +2,10 @@
 #'
 #' Run lmerel and corresponding lm or lme without kinship of gene expression in RNA-seq data against associated genotypes
 #'
-#' @param dat.snp Data frame containing 0,1,2 genotypes. Can be numeric or character/factor depending on desired analysis. If running contrasdts, must be character/factor for pairwise comparisons to run. Rows are donors and columns are SNP.
-#' @param dat.map Data frame mapping geneID to genotypeID. If provided, genotypes will be modeled against only the genes they are paired with in the map. If not provided, genotypes will be modeled against all genes in dat$E / counts
-#' @param geneID Character variable name to match dat.snp dat with gene identifiers. Values should match rownames in express data (dat$E or counts) Default it "gene"
-#' @param genotypeID Character variable name to match dat.snp dat with SNP identifiers. Default it "snpID"
+#' @param dat_snp Data frame containing 0,1,2 genotypes. Can be numeric or character/factor depending on desired analysis. If running contrasdts, must be character/factor for pairwise comparisons to run. Rows are donors and columns are SNP.
+#' @param dat_map Data frame mapping geneID to genotypeID. If provided, genotypes will be modeled against only the genes they are paired with in the map. If not provided, genotypes will be modeled against all genes in dat$E / counts
+#' @param geneID Character variable name to match dat_snp dat with gene identifiers. Values should match rownames in express data (dat$E or counts) Default it "gene"
+#' @param genotypeID Character variable name to match dat_snp dat with SNP identifiers. Default it "snpID"
 #' @param model Character vector of model starting with ~ Should include (1|patientID) if mixed effects will be run. NOTE THAT FOR EQTL, the model MUST include genotype as a place holder for the genotype data. For example "~ genotype + (1|ptID)"
 #'
 #' Same as kmFit( )
@@ -17,20 +17,32 @@
 #' @param meta Matrix or data frame of sample and individual metadata.
 #' @param genes Matrix or data frame of gene metadata.
 #' @param weights Matrix of data frame of gene specific weights. Usually calculated with limma::voomWithQualityWeights().
-#' @param subset.var Character list of variable name(s) to filter data by.
-#' @param subset.lvl Character list of variable value(s) or level(s) to filter data to. Must match order of subset.var
-#' @param subset.genes Character vector of genes to include in models.
-#' @param use.weights Logical if gene specific weights should be used in model. Default is FALSE
-#' @param run.lm Logical if should run lm model without kinship
-#' @param run.lme Logical if should run lme model without kinship
-#' @param run.lmerel Logical if should run lmerel model with kinship
-#' @param run.contrast Logical if should run pairwise contrasts. If no matrix provided, all possible pairwise comparisons are completed.
-#' @param contrast.var Character vector of variable in model to run contrasts of. Interaction terms must be specified as "var1:var2". If NULL (default), all contrasts for all variables in the model are run
+#' @param subset_var Character list of variable name(s) to filter data by.
+#' @param subset_lvl Character list of variable value(s) or level(s) to filter data to. Must match order of subset.var
+#' @param subset_genes Character vector of genes to include in models.
+#' @param use_weights Logical if gene specific weights should be used in model. Default is FALSE
+#' @param run_lm Logical if should run lm model without kinship
+#' @param run_lme Logical if should run lme model without kinship
+#' @param run_lmerel Logical if should run lmerel model with kinship
+#' @param run_contrast Logical if should run pairwise contrasts. If no matrix provided, all possible pairwise comparisons are completed.
+#' @param contrast_var Character vector of variable in model to run contrasts of. Interaction terms must be specified as "var1:var2". If NULL (default), all contrasts for all variables in the model are run
 #' @param metrics Logical if should calculate model fit metrics such as AIC, BIC, R-squared. Default is FALSE
 #' @param processors Numeric processors to run in parallel. Default is 2 less than the total available
-#' @param p.method Character of FDR adjustment method. Values as in p.adjust( )
-#' @param run.lmekin Depreciated. Please use run.lmerel
+#' @param p_method Character of FDR adjustment method. Values as in p.adjust( )
 #'
+#' @param dat.snp Deprecated form of dat_snp
+#' @param dat.map Deprecated form of dat.map
+#' @param run.lmekin Deprecated. Please use run_lmerel
+#' @param subset.var Deprecated form of subset_var
+#' @param subset.lvl Deprecated form of subset_lvl
+#' @param subset.genes Deprecated form of subset_genes
+#' @param use.weights Deprecated form of use_weights
+#' @param run.lm Deprecated form of run_lm
+#' @param run.lme Deprecated form of run_lme
+#' @param run.lmerel Deprecated form of run_lmerel
+#' @param run.contrast Deprecated form of run_contrast
+#' @param contrast.var Deprecated form of contrast_var
+#' @param p.method Deprecated form of p_method
 #' @return List of data frames including
 #'    - lm/lme/lmerel: model estimates and significance
 #'    - *.contrast: model estimates and significance for pairwise contrasts with variables in the original model
@@ -56,57 +68,83 @@
 #'                  gene = rep(example.voom$genes$geneName[1:2], nSNP/2))
 #'
 #' #Run SNP interaction model
-#' eQTL_result <- kmFit_eQTL(dat.snp = snp, dat.map = map,
+#' eQTL_result <- kmFit_eQTL(dat_snp = snp, dat_map = map,
 #'                           dat = example.voom, kin = example.kin,
-#'                           run.lmerel = TRUE, metrics=TRUE,
-#'                           run.contrast = TRUE, contrast.var="virus:genotype",
+#'                           run_lmerel = TRUE, metrics=TRUE,
+#'                           run_contrast = TRUE, contrast_var="virus:genotype",
 #'                           model = "~ virus*genotype + (1|ptID)")
 #'
 #' #Run SNP as factors with contrasts
 #' snp_fct <- snp %>%
 #'  dplyr::mutate_if(is.numeric, ~as.factor(.))
 #'
-#' eQTL_result2 <- kmFit_eQTL(dat.snp = snp_fct, dat.map = map,
+#' eQTL_result2 <- kmFit_eQTL(dat_snp = snp_fct, dat_map = map,
 #'                            dat = example.voom, kin = example.kin,
-#'                            run.lmerel = TRUE, run.contrast = TRUE, metrics=TRUE,
+#'                            run_lmerel = TRUE, run_contrast = TRUE, metrics=TRUE,
 #'                            model = "~ genotype + (1|ptID)")
 
-kmFit_eQTL <- function(dat.snp = NULL, dat.map = NULL,
+kmFit_eQTL <- function(dat_snp = NULL, dat_map = NULL,
                        geneID = "gene", genotypeID = "snpID",
                        #Same as kmFit
                        dat=NULL, kin=NULL, patientID="ptID", libraryID="libID",
                        counts=NULL, meta=NULL, genes=NULL, weights=NULL,
+                       subset_var = NULL, subset_lvl = NULL, subset_genes = NULL,
+                       model, use_weights=FALSE,
+                       run_lm = FALSE, run_lme = FALSE, run_lmerel = FALSE,
+                       metrics = FALSE,
+                       run_contrast = FALSE, contrast_var = NULL,
+                       processors = NULL, p_method = "BH",
+                       #deprecated
+                       dat.snp = NULL, dat.map = NULL,
+                       run.lmekin = NULL,
                        subset.var = NULL, subset.lvl = NULL, subset.genes = NULL,
-                       model, use.weights=FALSE,
+                       use.weights=FALSE,
                        run.lm = FALSE, run.lme = FALSE, run.lmerel = FALSE,
-                       metrics = FALSE, run.lmekin = NULL,
                        run.contrast = FALSE, contrast.var = NULL,
-                       processors = NULL, p.method = "BH"){
+                       p.method = NULL){
 
   snp <- genotype <- NULL
+  if(any(!is.null(subset.var),!is.null(subset.lvl),
+         !is.null(subset.genes),use.weights,run.lm,run.lme,
+         run.lmerel,run.contrast,!is.null(contrast.var),
+         !is.null(p.method),!is.null(dat.snp), !is.null(dat.map))){
+    message("WARNING: Arguments with '.' have been deprecated. Please use '_' versions.")
+  }
+  if(!is.null(dat.snp)){dat_snp <- dat.snp}
+  if(!is.null(dat.map)){dat_map <- dat.map}
+  if(!is.null(subset.var)){subset_var <- subset.var}
+  if(!is.null(subset.lvl)){subset_lvl <- subset.lvl}
+  if(!is.null(subset.genes)){subset_genes <- subset.genes}
+  if(use.weights){use_weights <- use.weights}
+  if(run.lm){run_lm <- run.lm}
+  if(run.lme){run_lme <- run.lme }
+  if(run.lmerel){run_lmerel <- run.lmerel}
+  if(run.contrast){run_contrast <- run.contrast}
+  if(!is.null(contrast.var)){contrast_var <- contrast.var}
+  if(!is.null(p.method)){p_method <- p.method}
 
   #### Checks ####
-  if(!is.null(dat.map) & !is.null(subset.genes)){
-    stop("Only one of dat.map and subset.genes can be provided.")}
-  if(!is.null(dat.map)){
-    if(!(genotypeID %in% colnames(dat.map))){
-      stop("genotypeID variable not in dat.map. Please set genotypeID to the correct variable name for your data.")}
-    if(!(geneID %in% colnames(dat.map))){
-      stop("geneID variable not in dat.map Please set geneID to the correct variable name for your data.")}}
+  if(!is.null(dat_map) & !is.null(subset_genes)){
+    stop("Only one of dat_map and subset.genes can be provided.")}
+  if(!is.null(dat_map)){
+    if(!(genotypeID %in% colnames(dat_map))){
+      stop("genotypeID variable not in dat_map. Please set genotypeID to the correct variable name for your data.")}
+    if(!(geneID %in% colnames(dat_map))){
+      stop("geneID variable not in dat_map Please set geneID to the correct variable name for your data.")}}
 
-  num.check <- as.matrix(dat.snp[,colnames(dat.snp) != patientID])
+  num.check <- as.matrix(dat_snp[,colnames(dat_snp) != patientID])
 
-  if((run.contrast | !is.null(contrast.var)) & is.numeric(num.check)){
-    message("WARNING: Genotype data are numeric. If you would like to treat 0,1,2 as distinct levels in the contrast model, please change dat.snp to character/factor.")
+  if((run_contrast | !is.null(contrast_var)) & is.numeric(num.check)){
+    message("WARNING: Genotype data are numeric. If you would like to treat 0,1,2 as distinct levels in the contrast model, please change dat_snp to character/factor.")
   }
 
   #### Data ####
-  #Move rownames if present in dat.snp
-  if(!patientID %in% colnames(dat.snp)){
-    snp.format <- dat.snp %>%
+  #Move rownames if present in dat_snp
+  if(!patientID %in% colnames(dat_snp)){
+    snp.format <- dat_snp %>%
       tibble::rownames_to_column(patientID)
   } else {
-    snp.format <- dat.snp
+    snp.format <- dat_snp
   }
 
 
@@ -131,28 +169,36 @@ kmFit_eQTL <- function(dat.snp = NULL, dat.map = NULL,
     m <- gsub("genotype", s, model)
 
     #Match SNP to subset of genes if within gene is selected
-    if(!is.null(dat.map)){
+    if(!is.null(dat_map)){
       #List genes of interest
-      subset.genes <- dat.map %>%
+      subset_genes <- dat_map %>%
         dplyr::filter(get(genotypeID) == s) %>%
         dplyr::pull(geneID)
     }
 
     #Fix contrast to specify SNP
-    if(!is.null(contrast.var)){
-      contrast.var2 <- gsub("genotype", s, contrast.var)
+    if(!is.null(contrast_var)){
+      contrast_var2 <- gsub("genotype", s, contrast_var)
     } else{
-      contrast.var2 <- contrast.var
+      contrast_var2 <- contrast_var
     }
 
     temp <- kmFit(dat, kin, patientID, libraryID,
                   counts, meta, genes, weights,
+                  subset_var, subset_lvl, subset_genes,
+                  model=m, use_weights,
+                  run_lm, run_lme, run_lmerel,
+                  metrics,
+                  run_contrast, contrast_var=contrast_var2,
+                  processors, p_method, genotype_name=s,
+                  #deprecated
+                  run.lmekin,
                   subset.var, subset.lvl, subset.genes,
-                  model=m, use.weights,
+                  use.weights,
                   run.lm, run.lme, run.lmerel,
-                  metrics, run.lmekin,
-                  run.contrast, contrast.var=contrast.var2,
-                  processors, p.method, genotype.name=s)
+                  run.contrast, contrast.var,
+                  p.method)
+
 
     #combine with previous snp outputs
     for(datf in unique(c(names(temp), names(result.ls)))){
